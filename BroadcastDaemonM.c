0a0e098008bb67a31919bf099da65f186551bc0b
rahultgeorge <rahultitusgeorge2011@gmail.com>
2019-11-13 16:37:30 -0500

    Daemon

--- BroadcastDaemonM.c
+++ BroadcastDaemonM.c
@@ -4,9 +4,19 @@
 
  #include "DBHandler.h"
 
+ /*Create a new thread which acts a client socket and sends the repliated write*/
+ void sendReplicatedWrite()
+ {
+   
+ }
+ 
  int main()
  {
-
+     while(1)
+     {
+         //read the signal and initiate the rep write
+         //
+     }
 
 
  }
